b506867b8fae86b03d370149ef23b5466fb310cf
A-Kittel <kittel609@gmail.com>
2021-07-27 22:27:17 +0300

    add basket logic && redis

---
+++ Core/Entities/CustomerBasket.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Entities
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; }
+        public List<BasketItems> Items { get; set; } = new List<BasketItems>();
+        
+        public CustomerBasket()
+        {
+        }
+        
+        public CustomerBasket(string id)
+        {
+            Id = id;
+        }
+    }
+}
